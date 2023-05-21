673dd79180062f1b72a39394676b012336c5874d
Joey Smith <jsmith@webinertia.net>
2023-05-21 17:36:11 -0500

    Adding Delegator factory for TemplateMapResolver This is to facilitate overriding the view_manager template map config in default builds so we do not need to modify the config. Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- config/module.config.php
+++ config/module.config.php
@@ -7,12 +7,15 @@ namespace Webinertia\ThemeManager;
 return [
     'service_manager' => [
         'aliases' => [
-            'ViewTemplatePathStack' => View\Resolver\TemplatePathStack::class,
+            'ViewTemplatePathStack'   => View\Resolver\TemplatePathStack::class,
         ],
         'factories' => [
-            View\Resolver\TemplatePathStack::class => ViewTemplatePathStackFactory::class
+            View\Resolver\TemplatePathStack::class => ViewTemplatePathStackFactory::class,
         ],
         'delegators' => [
+            'ViewTemplateMapResolver' => [
+                View\Resolver\TemplateMapFactory::class,
+            ],
             View\Resolver\TemplatePathStack::class => [
                 View\Resolver\TemplatePathStackFactory::class
             ],
---
+++ src/View/Resolver/TemplateMapFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webinertia\ThemeManager\View\Resolver;
+
+use Laminas\ServiceManager\Factory\DelegatorFactoryInterface;
+use Laminas\View\Resolver\TemplateMapResolver;
+use Psr\Container\ContainerInterface;
+
+class TemplateMapFactory implements DelegatorFactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $name, callable $callback, ?array $options = null): TemplateMapResolver
+    {
+        return new TemplateMapResolver([]);
+    }
+}
