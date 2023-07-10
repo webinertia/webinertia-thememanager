f3d3047982ea0cbeaa67cc52a50ad45b198f7d2e
Joey Smith <jsmith@webinertia.net>
2023-07-10 18:37:56 -0500

    Adds use for call_user_func in TemplateMapFactory Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/View/Resolver/TemplateMapFactory.php
+++ src/View/Resolver/TemplateMapFactory.php
@@ -8,11 +8,13 @@ use Laminas\ServiceManager\Factory\DelegatorFactoryInterface;
 use Laminas\View\Resolver\TemplateMapResolver;
 use Psr\Container\ContainerInterface;
 
+use call_user_func;
+
 class TemplateMapFactory implements DelegatorFactoryInterface
 {
     public function __invoke(ContainerInterface $container, $name, callable $callback, ?array $options = null): TemplateMapResolver
     {
-        $stack = \call_user_func($callback, []);
+        $stack = call_user_func($callback, []);
         $stack->setMap([]);
         return $stack;
     }
